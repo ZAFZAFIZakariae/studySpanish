55f1af6d67012fd2cebba69003c5276b9c0850bf
Zakariae <zakariaezafzafi@gmail.com>
2025-09-20 22:15:18 +0200

    Use remark-gfm compatible version

--- src/components/LessonViewer.tsx
+++ src/components/LessonViewer.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
 export const LessonViewer: React.FC<{ markdown: string }> = ({ markdown }) => {
   return (
     <div className="prose">
-      <ReactMarkdown>{markdown}</ReactMarkdown>
+      <ReactMarkdown remarkPlugins={[remarkGfm]}>{markdown}</ReactMarkdown>
     </div>
   );
 };
