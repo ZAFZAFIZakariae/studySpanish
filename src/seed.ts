1eb42e2634ee1e03e9dd56ae7b9ac4dcc691b523
Zakariae <zakariaezafzafi@gmail.com>
2025-09-20 16:32:04 +0200

    Seed initial content and refresh offline cache

--- src/seed.ts
+++ src/seed.ts
@@ -1,14 +1,8 @@
-import { db } from './db';
-import { LessonSchema, ExerciseSchema, FlashcardSchema } from './lib/schemas';
-
-export const importSeed = async (data: {
-  lessons: any[], exercises: any[], flashcards: any[]
-}) => {
-  const lessons = data.lessons.map(l => LessonSchema.parse(l));
-  const exercises = data.exercises.map(e => ExerciseSchema.parse(e));
-  const flashcards = data.flashcards.map(f => FlashcardSchema.parse(f));
-
-  await db.lessons.bulkPut(lessons);
-  await db.exercises.bulkPut(exercises);
-  await db.flashcards.bulkPut(flashcards);
-};
+export { importSeed } from './seed/importSeed';
+export {
+  ensureSeedData,
+  DEFAULT_SEED_VERSION,
+  SEED_VERSION_KEY,
+  CUSTOM_SEED_MARKER,
+  type SeedResult,
+} from './seed/ensureSeedData';
